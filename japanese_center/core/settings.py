38c62eb120b0dc724c8a5e321405a39212c05d00
Anh Tai <taih02551@gmail.com>
2026-03-02 21:52:56 +0700

    update khung và thêm requirement

--- japanese_center/core/settings.py
+++ japanese_center/core/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Japanese_Center_DB', # Tên database
+        'NAME': 'japanese_center_db', # Tên database
         'USER': 'root',
-        'PASSWORD': 'hha1525678',
+        'PASSWORD': 'admin123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
